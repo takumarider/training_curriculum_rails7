50665ce1848f4b41e2bd7a25a146177d7cbf6eaa
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-09 18:01:42 +0900

    修正：キー:」と「バリュー」の間に半角スペースを追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,9 +36,9 @@ class CalendarsController < ApplicationController
       end
       
       days = {
-        month:(@todays_date + x).month
-        date:(@todays_date + x).day
-        plans:today_plans
+        month: (@todays_date + x).month
+        date: (@todays_date + x).day
+        plans: today_plans
       }
       @week_days.push(days)
     end
